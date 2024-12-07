421cc3fa8b6eb58457e718625fc444219fea61e9
aadithpm <aadithpm@gmail.com>
2024-12-07 00:00:43 -0600

    ✨ First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lowkey
+
+`Lowkey` is a photography portfolio page with Vite, powered by serverless APIs built with Cloudflare Workers
---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+interface PortfolioProject {
+    projectName: string;
+    coverImageUrl: string;
+    imageUrls: string[];
+}
+
+function App() {
+    const [projectList, setProjects] = useState(Array<PortfolioProject>());
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(false);
+        setProjects([]);
+    }, []);
+    return (
+        <>
+            <div>
+                <div className="sidebar">
+                    <div className="sidebarTitle">lowkey</div>
+                    <ul className="sidebarProjectList">
+                        {projectList.map((item, index) => (
+                            <li key={index}>{item.projectName}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="gallery">
+                    <ul className="galleryProjectList">
+                        {projectList.map((item, index) => (
+                            <li key={index}>{item.projectName}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
